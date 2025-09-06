afa9badce45ea218007e4feeb88b7ee44adf3350
Li Xiaoyang <lxy2497685045@163.com>
2025-09-06 17:44:48 +0800

    项目整体路由设计

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,38 @@
+// createRouter：创建路由实例
+// createWebHistory：创建 history 模式的路由
 import { createRouter, createWebHistory } from 'vue-router'
+import Login from '@/views/Login/index.vue'
+import Layout from '@/views/Layout/index.vue'
+import Home from '@/views/Home/index.vue'
+import Category from '@/views/Category/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  // 配置path和component的映射关系的位置
+  routes: [
+    {
+      path: '/',
+      component: Layout,
+      children: [
+        {
+          // 默认二级路由，path配置项置空
+          path: '',
+          name: 'home',
+          component: Home,
+        },
+        {
+          path: 'category',
+          name: 'category',
+          component: Category,
+        },
+      ],
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+    },
+  ],
 })
 
 export default router
