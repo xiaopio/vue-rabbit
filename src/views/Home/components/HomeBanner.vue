2e67ef757c5953e2cb8693fb6bfdb9c7253c5a80
Li Xiaoyang <lxy2497685045@163.com>
2025-09-07 14:54:24 +0800

    home-banner轮播图

--- src/views/Home/components/HomeBanner.vue
+++ src/views/Home/components/HomeBanner.vue
@@ -1,3 +1,44 @@
+<script setup>
+import { onMounted, ref } from "vue";
+import { getBannerAPI } from "@/apis/home.js";
+
+const bannerList = ref([]);
+const getBanner = async () => {
+  const res = await getBannerAPI();
+  // console.log(res);
+  // console.log(res.result);
+  bannerList.value = res.result;
+}
+
+onMounted(() => getBanner())
+</script>
+
+
+
 <template>
-  我是banner
+  <div class="home-banner">
+    <el-carousel height="500px">
+      <el-carousel-item v-for="item in bannerList" :key="item">
+        <img :src="item.imgUrl" alt="">
+      </el-carousel-item>
+    </el-carousel>
+  </div>
 </template>
+
+
+
+<style scoped lang='scss'>
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 98;
+
+  img {
+    width: 100%;
+    height: 500px;
+  }
+}
+</style>
