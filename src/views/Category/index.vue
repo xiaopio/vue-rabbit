a3aded02ffab55329c9e9985beedec8c2599d969
Li Xiaoyang <lxy2497685045@163.com>
2025-09-07 20:42:42 +0800

    二级分类-整体认识和路由配置

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -13,7 +13,7 @@ const { categoryData } = useCategory();
       <!-- 面包屑 -->
       <div class="bread-container">
         <el-breadcrumb separator=">">
-          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item :to="{ path: '' }">首页</el-breadcrumb-item>
           <!-- <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item> -->
           <el-breadcrumb-item>{{ categoryData?.name }}</el-breadcrumb-item>
         </el-breadcrumb>
@@ -30,7 +30,7 @@ const { categoryData } = useCategory();
         <h3>全部分类</h3>
         <ul>
           <li v-for="i in categoryData?.children" :key="i.id">
-            <RouterLink to="/">
+            <RouterLink :to="`/category/sub/${i.id}`">
               <img :src="i.picture" />
               <p>{{ i.name }}</p>
             </RouterLink>
