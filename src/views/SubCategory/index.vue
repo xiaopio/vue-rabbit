717affd016c9b20b565ad286339b68365e1438f0
Li Xiaoyang <lxy2497685045@163.com>
2025-09-07 21:47:54 +0800

    二级列表-筛选功能实现

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -31,6 +31,13 @@ const getGoodList = async () => {
 onMounted(() => {
   getGoodList()
 })
+
+// tab切换回调
+const tabChange = () => {
+  console.log(reqData.value.sortField)
+  reqData.value.page = 1
+  getGoodList()
+}
 </script>
 
 <template>
@@ -45,7 +52,7 @@ onMounted(() => {
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
