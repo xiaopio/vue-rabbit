bf397d7f25d914881e5e362a426a85963ba994d2
Li Xiaoyang <lxy2497685045@163.com>
2025-09-07 19:25:31 +0800

    导航栏激活状态显示及分类列表渲染

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -2,6 +2,7 @@
 import { getTopCategoryAPI } from "@/apis/category"
 import { ref, onMounted, watch } from "vue"
 import { useRoute } from "vue-router"
+import GoodsItem from "@/views/Home/components/GoodsItem.vue"
 const route = useRoute();
 // 获取数据
 const categoryData = ref(null);
@@ -24,10 +25,14 @@ const getBanner = async () => {
   bannerList.value = res.result;
 }
 
-onMounted(() =>
-  getCategoryData(),
-  getBanner()
-)
+// 修正 onMounted 写法：单个回调函数内执行所有初始化请求
+onMounted(async () => {
+  // 可选：并行请求（提高加载效率，两个请求互不依赖）
+  await Promise.all([
+    getCategoryData(),
+    getBanner()
+  ]);
+});
 
 // 监听路由参数变化（如果页面可能在同路由下参数变化）
 watch(() => route.params.id, () => {
@@ -53,6 +58,25 @@ watch(() => route.params.id, () => {
             <img :src="item.imgUrl" alt="">
           </el-carousel-item>
         </el-carousel>
+      </div>
+      <div class="sub-list">
+        <h3>全部分类</h3>
+        <ul>
+          <li v-for="i in categoryData?.children" :key="i.id">
+            <RouterLink to="/">
+              <img :src="i.picture" />
+              <p>{{ i.name }}</p>
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+      <div class="ref-goods" v-for="item in categoryData?.children" :key="item.id">
+        <div class="head">
+          <h3>- {{ item.name }}-</h3>
+        </div>
+        <div class="body">
+          <GoodsItem v-for="good in item.goods" :goods="good" :key="good.id" />
+        </div>
       </div>
     </div>
   </div>
