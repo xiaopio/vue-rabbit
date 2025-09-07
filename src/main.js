8fe7b14a54b8d313e4a8a2a0c62c31ad5121608f
Li Xiaoyang <lxy2497685045@163.com>
2025-09-07 16:06:56 +0800

    home-图片懒加载实现

--- src/main.js
+++ src/main.js
@@ -19,3 +19,24 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+// 定义全局指令
+import { useIntersectionObserver } from '@vueuse/core'
+app.directive('img-lazy', {
+  mounted(el, binding) {
+    // el: 指令绑定的元素
+    // binding: binding.value 指令等于号后面绑定的表达式的值
+    // console.log(el, binding)
+    const { stop } = useIntersectionObserver(
+      el,
+      ([{ isIntersecting }]) => {
+        // console.log(isIntersecting)
+        if (isIntersecting) {
+          // 进入视口区域
+          el.src = binding.value
+          stop()
+        }
+      },
+    )
+  }
+})
