85d49abe03b76be674a1341d4df04021f5e7ed3a
Li Xiaoyang <lxy2497685045@163.com>
2025-09-08 21:27:30 +0800

    状态栏-登录和非登录状态下的模板适配

--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -1,5 +1,7 @@
 <script setup>
-
+import { useUserStore } from '@/stores/user'
+const userStore = useUserStore()
+const { userInfo } = userStore
 </script>
 
 <template>
@@ -7,8 +9,8 @@
     <div class="container">
       <ul>
         <!-- 多模板渲染 用来区分登录状态和非登录状态 -->
-        <template v-if="false">
-          <li><a href="javascript:;"><i class=" iconfont icon-user"></i>周杰伦</a></li>
+        <template v-if="userInfo.token">
+          <li><a href="javascript:;"><i class=" iconfont icon-user"></i>{{ userInfo.nickname }}</a></li>
           <li>
             <el-popconfirm title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
               <template #reference>
