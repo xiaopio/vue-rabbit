50433f4db77acfb025319f722513c1f62f2501e5
Li Xiaoyang <lxy2497685045@163.com>
2025-09-08 18:58:28 +0800

    登录页-表单校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,36 @@
 <script setup>
+import { ref } from 'vue';
+// 表单校验
 
+// 准备表单对象
+const form = ref({
+  account: '',
+  password: ''
+})
+
+// 规则对象
+const rules = {
+  account: [
+    {
+      required: true,
+      message: '用户名不能为空',
+      trigger: 'blur'
+    }
+  ],
+  password: [
+    {
+      required: true,
+      message: '密码不能为空',
+      trigger: 'blur'
+    },
+    {
+      min: 6,
+      max: 14,
+      message: '密码长度6-14位',
+      trigger: 'blur'
+    }
+  ]
+}
 </script>
 
 
@@ -20,16 +51,16 @@
     <section class="login-section">
       <div class="wrapper">
         <nav>
-          <a href="javascript:;">账户登录</a>
+          <a href="javascript:;">账号登录</a>
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form label-position="right" label-width="60px" status-icon>
-              <el-form-item label="账户">
-                <el-input />
+            <el-form :model="form" :rules="rules" label-position="right" label-width="60px" status-icon>
+              <el-form-item prop="account" label="账号">
+                <el-input v-model="form.account" placeholder="请输入账号" />
               </el-form-item>
-              <el-form-item label="密码">
-                <el-input />
+              <el-form-item prop="password" label="密码">
+                <el-input v-model="form.password" placeholder="请输入密码" show-password />
               </el-form-item>
               <el-form-item label-width="22px">
                 <el-checkbox size="large">
