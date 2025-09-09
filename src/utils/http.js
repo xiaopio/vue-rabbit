09bccba7387b5c35bdda8ba610344c0b811aef9d
Li Xiaoyang <lxy2497685045@163.com>
2025-09-09 10:26:37 +0800

    axios-Token失效401拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -4,6 +4,7 @@ import { ElMessage } from 'element-plus'
 import 'element-plus/dist/index.css'
 
 import { useUserStore } from '@/stores/user'
+import router from '@/router'
 
 const httpInstance = axios.create({
   baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -26,8 +27,17 @@ httpInstance.interceptors.request.use(config => {
 
 // axios响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
+  const userStore = useUserStore()
   // 统一错误提示
   ElMessage.error(e.response.data.message)
+  // 401token失效处理
+  if (e.response && e.response.status === 401) {
+    // 1、清除本地用户数据
+    userStore.clearUserInfo()
+    // 2、跳转到登录页面，同时记录当前路径用于登录后跳转回来
+    router.push('/login')
+  }
+
   return Promise.reject(e)
 })
 
