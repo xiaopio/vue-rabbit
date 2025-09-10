a62999f58294ec18a68790389e4ba1e168fcf490
Li Xiaoyang <lxy2497685045@163.com>
2025-09-10 16:13:51 +0800

    结算页-基础数据渲染

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import Category from '@/views/Category/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
 import Detail from '@/views/Detail/index.vue'
 import CartList from '@/views/CartList/index.vue'
+import Checkout from '@/views/Checkout/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -42,7 +43,12 @@ const router = createRouter({
           path: 'cartlist',
           name: 'cartlist',
           component: CartList,
-        }
+        },
+        {
+          path: 'checkout',
+          name: 'checkout',
+          component: Checkout,
+        },
       ],
     },
     {
