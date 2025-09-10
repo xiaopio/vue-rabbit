92ca3191b7d5bc48cfd526c891e897c13c6b3517
Li Xiaoyang <lxy2497685045@163.com>
2025-09-10 16:34:08 +0800

    结算页-切换默认地址功能实现

--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -7,14 +7,26 @@ const getCheckInfo = async () => {
   const res = await getCheckInfoAPI()
   checkInfo.value = res.result
   // 获取默认地址
-  curAddress.value = checkInfo.value.userAddresses.find(i => i.isDefault ===0)
+  curAddress.value = checkInfo.value.userAddresses.find(i => i.isDefault === 0)
 
 }
 
 onMounted(() => {
   getCheckInfo()
 })
+// 控制切换地址弹窗
+const showDialog = ref(false)
+// 切换地址
+const activeAddress = ref({})
+const switchAddress = (item) => {
+  activeAddress.value = item
 
+}
+
+const confirm = () => {
+  curAddress.value = activeAddress.value
+  showDialog.value = false
+}
 
 </script>
 
@@ -35,7 +47,7 @@ onMounted(() => {
               </ul>
             </div>
             <div class="action">
-              <el-button size="large" @click="toggleFlag = true">切换地址</el-button>
+              <el-button size="large" @click="showDialog = true">切换地址</el-button>
               <el-button size="large" @click="addFlag = true">添加地址</el-button>
             </div>
           </div>
@@ -116,6 +128,24 @@ onMounted(() => {
     </div>
   </div>
   <!-- 切换地址 -->
+  <el-dialog v-model="showDialog" title="切换收货地址" width="30%" center>
+    <div class="addressWrapper">
+      <div class="text item" :class="{ active: item.id === activeAddress.id }" @click="switchAddress(item)"
+        v-for="item in checkInfo.userAddresses" :key="item.id">
+        <ul>
+          <li><span>收<i />货<i />人：</span>{{ item.receiver }} </li>
+          <li><span>联系方式：</span>{{ item.contact }}</li>
+          <li><span>收货地址：</span>{{ item.fullLocation + item.address }}</li>
+        </ul>
+      </div>
+    </div>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button>取消</el-button>
+        <el-button type="primary" @click="confirm()">确定</el-button>
+      </span>
+    </template>
+  </el-dialog>
   <!-- 添加地址 -->
 </template>
 
