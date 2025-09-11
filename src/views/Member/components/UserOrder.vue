ab26d787581175b11f9408a062e04fbcb516b980
Li Xiaoyang <lxy2497685045@163.com>
2025-09-11 11:46:53 +0800

    会员中心-我的订单-使用tab栏切换获取状态数据

--- src/views/Member/components/UserOrder.vue
+++ src/views/Member/components/UserOrder.vue
@@ -1,4 +1,7 @@
 <script setup>
+import { getUserOrder } from "@/apis/order"
+import { get } from "@vueuse/core";
+import { onMounted, ref } from "vue";
 // tab列表
 const tabTypes = [
   { name: "all", label: "全部订单" },
@@ -10,13 +13,33 @@ const tabTypes = [
   { name: "cancel", label: "已取消" }
 ]
 // 订单列表
-const orderList = []
+const orderList = ref([])
+const params = ref({
+  orderState: 0,
+  page: 1,
+  pageSize: 2
+})
+const getOrderList = async () => {
+  const res = await getUserOrder(params.value)
+  orderList.value = res.result.items
+}
+
+onMounted(() => {
+  getOrderList()
+})
+
+// tab切换
+const tabChange = (type) => {
+  // console.log(type)
+  params.value.orderState = type
+  getOrderList()
+}
 
 </script>
 
 <template>
   <div class="order-container">
-    <el-tabs>
+    <el-tabs @tab-change="tabChange">
       <!-- tab切换 -->
       <el-tab-pane v-for="item in tabTypes" :key="item.name" :label="item.label" />
 
